22102a11f756933f1583ff13143ec1a36d752ac4
javimt <javiergp_19@outlook.com>
2023-02-03 15:27:19 -0500

    validations form

--- client/src/views/Form.jsx
+++ client/src/views/Form.jsx
@@ -6,11 +6,21 @@ import { getTemperaments, createDogs } from "../redux/action";
 
 
 export default function Form() {
+
+  // Un formulario controlado con JavaScript con los siguientes campos:
+  // Nombre
+  // Altura (Diferenciar entre altura mínima y máxima)
+  // Peso (Diferenciar entre peso mínimo y máximo) 
+  // Años de vida
+  // Posibilidad de seleccionar/agregar uno o más temperamentos
+  // Botón/Opción para crear una nueva raza de perro
+
   const dispatch = useDispatch();
   const dogs = useSelector(state => state.allDogs);
   const tempers = useSelector(state => state.tempers);
   const [button, setButton] = useState(true)
 //console.log(tempers)
+
   const [errors, setErrors] = useState({
     name: "",
     image: "",
@@ -81,6 +91,11 @@ console.log(value);
     })
   }
 
+  function handleError(e) {
+    e.preventDefault();
+    alert("Complete the form!");
+  }
+
 //============================>> END HANDLERS <<==============================\\
 
 
@@ -88,22 +103,39 @@ console.log(value);
 
 const validate = (input) => {
   const errors = {};
+
+  //=======>> NAME <<======\\
   if(!input.name) {
     errors.name = "You must enter a breed name";
   } else if(!/^[a-zA-Z]+$/.test(input.name)) {
+    errors.name = "Invalid name. The name must contain letters";
+  } else if(input.name.length < 2 || input.name.length > 20) {
+    errors.name = "At least 2 letters, less than 20";
+  } else if(dogs.includes(input.name)) {
+    errors.name = "The dog already exists, use another name";
+  };
+  //=======>> END NAME <<======\\
 
+  //=======>> HEIGHT <<======\\
+  if(!input.height.length < 2) {
+    errors.height = "You must enter a minimum and maximum height";
+  } else if(!/^[0-9]+$/.test(input.height)) {
+    errors.height = "Can only contain numbers"
   }
+  //=======>> END HEIGHT <<======\\
+  
+  if(!input.weight.length < 2) {
+    errors.weight = "You must enter a minimum and maximum weight";
+  }else if(!/^[0-9]+$/.test(input.weight)) {
+    errors.weight = ""
 
+  };
 
-  if(!input.height.length < 2) errors.height = "You must enter a minimum and maximum height";
-  if(!input.weight.length < 2) errors.weight = "You must enter a minimum and maximum weight";
   if(!input.life_span) errors.life_span = "You must enter a fife span";
   return errors;
 }
 
 //============================>> END VALIDATIONS <<==============================\\
-
-
 
   return (
     <div className={style.container}>
@@ -154,6 +186,27 @@ const validate = (input) => {
               </div>
             </div>
           </div>
+          <div className={style.dog}>
+            <div className={style.weight}>
+              <div className={style.div}>
+                <label className={style.title} htmlFor="weight">Weight: </label>
+                <input 
+                  className={errors.weight && style.error}
+                  type="number" 
+                  name="weight"
+                  min="0"
+                  max="200"
+                  value={input.weight}
+                  onChange={handlerChange}
+                />
+              </div>
+            </div>
+          </div>
+          {
+            input.name === "" ? (
+              <button className={style.btn2} type="submit" onClick={handleError}>Create</button>
+            ) : <button className={style.btn2} type="submit" >Create</button>
+          }
         </form>
       </div>
     </div>
