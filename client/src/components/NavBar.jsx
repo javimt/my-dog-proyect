96f5b6a09ad68ccec19062df9b39f3aa01a6ac43
javimt <javiergp_19@outlook.com>
2023-02-07 00:44:51 -0500

    css ok

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,17 +2,34 @@ import { Link } from 'react-router-dom';
 import style from '../styles/NavBar.module.css';
 import SearchBar from './SearchBar.jsx';
 import dog from '../images/PERROS (313).gif'
+import { useDispatch } from 'react-redux';
+import { getDogs } from '../redux/action';
+//import { useHistory } from 'react-router-dom';
 
 export default function NavBar() {
+  const dispatch = useDispatch();
+  //const history = useHistory();
+
+  function handlerReset(e) {
+    e.preventDefault();
+    dispatch(getDogs())
+    //history.push('/home')
+  }
+
   return (
     <nav className={style.nav} >
+      <div className={style.dog}>
+        <img className={style.img} src={dog} alt="" />
+      </div>
       <div className={style.create}>
         <Link to='/form' >
           <button className={style.link}>Create</button>
         </Link>
       </div>
-      <div className={style.dog}> 
-        <img className={style.img} src={dog} alt="" />
+      <div className={style.reset}>
+        <Link to='/home'>
+          <button className={style.home} onClick={handlerReset} type="submit">Home</button>
+        </Link>
       </div>
       <div className={style.search}>
         <SearchBar />
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import style from '../styles/SearchBar.module.css';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useState } from "react";
-import { getDogs, getDogsByName } from "../redux/action";
+import { getDogsByName } from "../redux/action";
 
 export default function SearchBar() {
   const dispatch = useDispatch();
@@ -12,7 +12,7 @@ export default function SearchBar() {
 
   function handleName(e) {
     e.preventDefault();
-    setName(e.target.value);
+    setName(e.target.value); 
   }
 
   function handleSubmit(e) {
@@ -30,16 +30,13 @@ export default function SearchBar() {
     //}
   }
   
-  function handleReset(e) {
+  /* function handleReset(e) {
     e.preventDefault();
     dispatch(getDogs())
-  }
+  } */
 
   return (
     <div className={style.bar}>
-      <div>
-        <button className={style.home} onClick={handleReset} type="submit">Home</button>
-      </div>
       <input /* className={style.input} */ value={name} type="text" placeholder="Write your breed" onChange={handleName}/>
       <button /*className={style.button}*/ type="submit" onClick={handleSubmit} > Search </button>
     </div>
